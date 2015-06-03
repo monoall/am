a40f5a213e3144f9feb6603fa9ad813b708ae0f0
Igor Shvedenenko <igor_sh@inisoft.com.ua>
2015-06-03 22:17:27 +0300

    sqLite finished with robolectric

--- automessenger/app/src/main/java/ua/org/javatraining/automessenger/app/database/Subsription.java
+++ automessenger/app/src/main/java/ua/org/javatraining/automessenger/app/database/Subsription.java
@@ -1,17 +1,60 @@
 package ua.org.javatraining.automessenger.app.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 /**
  * Created by berkut on 29.05.15.
  */
-public class Subsription {
+public class Subsription implements DbConstants {
 
+    private static SQLiteDatabase sqLiteDatabase;
     private SQLiteAdapter sqLiteAdapter;
+    private int idUser;
+    private int idTag;
 
-    public Subsription(Context context){
+    public Subsription(Context context, int idUser, int idTag){
         sqLiteAdapter = SQLiteAdapter.initInstance(context);
-        sqLiteAdapter.openToWrite();
-        //sqLiteAdapter.insertToSubscription();
+        this.idUser = idUser;
+        this.idTag = idTag;
+        insertToSubscription(idUser, idTag);
     }
+
+
+    /**
+     * Записывает в таблицу Subscription информацию о подписках пользователя
+     * @param idUser Id юзера
+     * @param idTag  Id тега
+     * @return позицию в таблице
+     */
+    protected long insertToSubscription(int idUser, int idTag) {
+        sqLiteDatabase = sqLiteAdapter.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(USER_ID, idUser);
+        cv.put(TAG_ID, idTag);
+        return sqLiteDatabase.insert(SUBSCRIPTION_TABLE, null, cv);
+    }
+
+    protected int querryIdFromSubscription(int idUser) {
+        sqLiteDatabase = sqLiteAdapter.getReadableDatabase();
+        String id = String.valueOf(idUser);
+        String sqlQuery = "SELECT ID from SUBSCRIPTION where ID_USER = ?";
+        Cursor cursor = sqLiteDatabase
+                .rawQuery(sqlQuery, new String[]{id});
+        int indexId = cursor.getColumnIndex(ID);
+        return cursor.getInt(indexId);
+    }
+
+    /**
+     * Удаляет подписку из Subscription
+     * @param idSubscription Id Subscription
+     */
+    protected void deleteSubscription(int idSubscription){
+        sqLiteDatabase = sqLiteAdapter.getReadableDatabase();
+        sqLiteDatabase.delete(SUBSCRIPTION_TABLE, "ID = " + idSubscription, null);
+    }
+
+
 }
--- automessenger/app/src/main/java/ua/org/javatraining/automessenger/app/database/Tag.java
+++ automessenger/app/src/main/java/ua/org/javatraining/automessenger/app/database/Tag.java
@@ -1,20 +1,76 @@
 package ua.org.javatraining.automessenger.app.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 /**
  * Created by berkut on 29.05.15.
  */
-public class Tag {
+public class Tag implements DbConstants {
 
+    private static SQLiteDatabase sqLiteDatabase;
     private String nameTag;
-    private SQLiteAdapter sqLiteAdapter;
+    private static SQLiteAdapter sqLiteAdapter;
 
     public Tag(Context context, String nameTag){
         this.nameTag = nameTag;
         sqLiteAdapter = SQLiteAdapter.initInstance(context);
         sqLiteAdapter.openToWrite();
-        sqLiteAdapter.insertToTag(nameTag);
+        insertToTag(nameTag);
+    }
+
+    /**
+     * Записывает в таблицу Tag новый тег
+     * @param tagName имя тега
+     * @return позицию в таблице
+     */
+    private long insertToTag(String tagName) {
+        sqLiteDatabase = sqLiteAdapter.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(TAG_NAME, tagName);
+        return sqLiteDatabase.insert(TAG_TABLE, null, cv);
+    }
+
+    /**
+     * Выбирает ID тега по его имени
+     * @param tagName имя юзера
+     * @return Id тега или -1, если такого тега не существует в таблице
+     */
+    protected static int querryIdFromTag(String tagName) {
+        sqLiteDatabase = sqLiteAdapter.getReadableDatabase();
+        String sqlQuery = "SELECT ID from TAG where TAG_NAME = ?";
+        Cursor cursor = sqLiteDatabase
+                .rawQuery(sqlQuery, new String[]{tagName});
+        int indexId = cursor.getColumnIndex(ID);
+        return cursor.getInt(indexId);
+
+    }
+
+    /**
+     * Удаляет тег
+     */
+    protected void deleteTag(){
+        sqLiteDatabase = sqLiteAdapter.getReadableDatabase();
+        int id = querryIdFromTag(nameTag);
+        sqLiteDatabase.delete(TAG_TABLE, "ID = " + id, null);
+    }
+
+
+    /**
+     * Выбирает и упаковывает тег в объект
+     * @param context контекст
+     * @param tagName имя тега
+     * @return объект Tag
+     */
+    public static Tag getTag(Context context, String tagName){
+        sqLiteDatabase = sqLiteAdapter.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.query(TAG_TABLE, null,
+                null, null, null, null, null);
+        int indexTagName = cursor.getColumnIndex(TAG_NAME);
+        cursor.move(querryIdFromTag(tagName));
+        return new Tag(context, cursor.getString(indexTagName));
     }
 
 }
